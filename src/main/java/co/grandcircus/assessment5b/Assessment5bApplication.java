64dc14fbe7f22321844e67b1e65da931b3acd5d5
Antonella Solomon <antonellasolomon@gmail.com>
2019-08-21 19:32:12 -0400

    initial commit assessment5b starter july 2019

---
+++ src/main/java/co/grandcircus/assessment5b/Assessment5bApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.assessment5b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assessment5bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assessment5bApplication.class, args);
+	}
+
+}
